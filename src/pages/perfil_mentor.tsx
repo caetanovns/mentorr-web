822461fd24ff3014cb5cffa1ae9fa4aa85957577
caetanovns <caetanov120@gmail.com>
2025-03-07 23:38:04 -0300

    adicionado laravel echo

--- src/pages/perfil_mentor.tsx
+++ src/pages/perfil_mentor.tsx
@@ -1,8 +1,30 @@
 import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import echo from '../config/echo';
+import { toast, ToastContainer } from "react-toastify";
 
 function PerfilMentor() {
+
+  const [texto, setTexto] = useState();
+
+  useEffect(() => {
+    console.log("iniciou componente");
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    echo.channel('channel-enroll').listen('EnrollEvent', (event: any) => {
+      const data = JSON.parse(event.data);
+      setTexto(data.students);
+      toast("Novo aluno matriculado!");
+    });
+
+    return () => {
+      echo.leave('channel-enroll');
+      echo.disconnect();
+    };
+  },[]);
+
+
   const { id } = useParams();
-  return <p>Perfil ID {id}</p>;
+  return <><p>User: {id} - atualizando em tempo real...: {texto}</p><ToastContainer /></>;
 }
 
 export default PerfilMentor;
